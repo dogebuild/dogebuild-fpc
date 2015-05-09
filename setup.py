160b5bdff0e32b6f47a90a3418aeda38fa9f206f
kirillsulim <kirillsulim@gmail.com>
2015-05-09 13:14:03 +0500

    Add travis and classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dogebuild-fpc',
-    version='0.1',
+    version='0.1.0.dev1',
     description='Free Pascal Compiler plugin for dogebuild',
     author='Kirill Sulim',
     author_email='kirillsulim@gmail.com',
@@ -10,4 +10,16 @@ setup(
     packages=find_packages(include=[
         'dogebuild*',
       ]),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+    ],
+    keywords='dogebuild builder fpc pascal',
 )
